de1023a406231eb0f60f7f86edce2053336ccefe
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-09-17 18:06:19 +0300

    create flatter 1

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -16,3 +16,16 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
 
 def unique_sorted(nums: list[float | int]) -> list[float | int]:
     return sorted(set(nums))
+
+
+def flatten(mat: list[list | tuple]) -> list:
+    array = list()
+    for arr in mat:
+        if not (isinstance(arr, tuple) or isinstance(arr, list)):
+            return TypeError
+        for member in arr:
+            array.append(member)
+    return array
+
+
+print(flatten([[1, 2], "ab"]))
