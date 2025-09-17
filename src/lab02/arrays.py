807b55dec1a1968a56d90286a6bf1b3afb0d924d
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-09-17 17:58:01 +0300

    create min_max & unique_sorted

---
+++ src/lab02/arrays.py
@@ -0,0 +1,18 @@
+def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
+    if len(nums) == 0:
+        return ValueError
+
+    mi = 9e6
+    ma = -9e6
+
+    for i in range(len(nums)):
+        if nums[i] < mi:
+            mi = nums[i]
+        if nums[i] > ma:
+            ma = nums[i]
+
+    return tuple([mi, ma])
+
+
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    return sorted(set(nums))
