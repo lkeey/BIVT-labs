2c7b976dce5410079c2a4b42721325204dc30ec5
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-10-11 23:25:36 +0300

    update doc-strings for json_csv

--- src/lab05/cvs_xlsx.py
+++ src/lab05/cvs_xlsx.py
@@ -9,13 +9,20 @@ def csv_to_xlsx(
     xlsx_path: str | Path,
     encoding: str = "utf-8",
 ) -> None:
+    csv_path = Path(csv_path)
+
+    if not csv_path.exists():
+        raise FileNotFoundError(f"CSV-файл {csv_path} не найден")
+
     wb = Workbook()
     ws = wb.active
     ws.title = "Sheet1"
 
-    csv_path = Path(csv_path)
     with csv_path.open("r", encoding=encoding) as csv_file:
         reader = csv.DictReader(csv_file)
+
+        if not reader.fieldnames:
+            raise ValueError("CSV без заголовков или пуст")
 
         ws.append(reader.fieldnames)
 
--- src/lib/json_csv.py
+++ src/lib/json_csv.py
@@ -21,11 +21,29 @@ def json_to_csv(
         - Определяет заголовки по ключам первого словаря.
         - Создаёт CSV с заголовком и строками из JSON.
 
+    Падает с:
+        FileNotFoundError: Если JSON-файл отсутствует.
+        ValueError: Если JSON пустой, не является списком словарей
+                    или имеет неподдерживаемую структуру.
+                    Если JSON-файл содержит синтаксические ошибки.
     """
 
     json_path = Path(json_path)
+
+    if not json_path.exists():
+        raise FileNotFoundError(f"Файл {json_path} не найден")
+
     with json_path.open("r", encoding=encoding) as json_file:
-        data_json = json.load(json_file)
+        try:
+            data_json = json.load(json_file)
+        except json.JSONDecodeError:
+            raise ValueError("Пустой JSON или неподдерживаемая структура")
+
+    if not data_json or not isinstance(data_json, list):
+        raise ValueError("Пустой JSON или неподдерживаемая структура")
+
+    if not all(isinstance(row, dict) for row in data_json):
+        raise ValueError("JSON должен содержать список словарей")
 
     csv_path = Path(csv_path)
     ensure_parent_dir(csv_path)
@@ -50,12 +68,26 @@ def csv_to_json(
         - Читает CSV с заголовком.
         - Преобразует строки CSV в список словарей.
         - Записывает JSON с отступами для удобного чтения.
+
+    Падает с:
+        FileNotFoundError: Если CSV-файл отсутствует.
+        ValueError: Если CSV не содержит заголовков или пуст.
+                    Если структура CSV-файла некорректна.
     """
 
     csv_path = Path(csv_path)
+
+    if not csv_path.exists():
+        raise FileNotFoundError(f"CSV-файл {csv_path} не найден")
+
     with csv_path.open("r", encoding=encoding) as csv_file:
         reader = csv.DictReader(csv_file)
+        if not reader.fieldnames:
+            raise ValueError("CSV-файл не содержит заголовков или пуст")
         data_csv = [row for row in reader]
+
+    if not data_csv:
+        raise ValueError("CSV-файл пуст")
 
     json_path = Path(json_path)
     ensure_parent_dir(json_path)
